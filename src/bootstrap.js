03008955c9957f29f8b5ba423b45e47c1e014bbf
Matt Crowder <matt.crowder@veeva.com>
2022-11-03 13:04:09 -0500

    ids

---
+++ src/bootstrap.js
@@ -0,0 +1,8 @@
+import ReactDOM from "react-dom"
+import App from "./module-federation-test"
+
+const container = document.getElementById("root")
+
+const root = ReactDOM.createRoot(container)
+
+root.render(<App />)
--- src/module-federation-test.js
+++ src/module-federation-test.js
@@ -2,9 +2,10 @@ import React from "react"
 
 const ModuleFederationTest = () => {
   const id = React.useId()
+  const id2 = React.useId()
   return (
     <div>
-      I am a federated module component <p>id: {id}</p>
+      I am a federated module component <p>id: {id}</p> <p>id2: {id2}</p>
     </div>
   )
 }
