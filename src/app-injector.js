2e1c7b5ec71b4d3805122d2fe5edcfaa6a345942
Matt Crowder <matt.crowder@veeva.com>
2022-11-17 11:00:18 -0600

    add callbacks

--- src/app-injector.js
+++ src/app-injector.js
@@ -2,10 +2,10 @@ import App from "./module-federation-test";
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-export const inject = parentElementId => {
+export const inject = (parentElementId, callback) => {
   const node = document.getElementById(parentElementId)
   const root = createRoot(node)
-  root.render(<App/>)
+  root.render(<App callback={callback}/>)
   return () => root.unmount()
 }
 
--- src/module-federation-test.js
+++ src/module-federation-test.js
@@ -1,11 +1,12 @@
 import React from "react"
 import ReactAdapterProvider from "src/react-adapter-provider"
 
-const ModuleFederationTest = () => {
+const ModuleFederationTest = (props) => {
   const id = React.useId()
   const id2 = React.useId()
   return (
     <div>
+      <button onClick={props.callback}>callback</button>
       I am a federated module component and I'm using React 18 features <p>id: {id}</p> <p>id2: {id2}</p>
     </div>
   )
